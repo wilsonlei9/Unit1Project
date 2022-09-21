bed1d03bff3f5a182ee2d89c22fc4928b4eeb44b
BT_4N2_22 <wlei6251@bths.edu>
2022-09-21 12:14:05 -0400

    Rounded values

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -14,10 +14,13 @@ public class TipCalculator {
         double tipPerPerson = totalTip/party;
         double totalPerPerson = totalBillWithTip/party;
         double roundTotalTip = (double)Math.round(totalTip*100)/100;
-        System.out.println("Total tip: " + roundTotalTip);
-        System.out.println("Total bill including tip: " + totalBillWithTip);
-        System.out.println("Tip per person:" + tipPerPerson);
-        System.out.println("Total per person:" + totalPerPerson);
+        double roundTotalBillWithTip = (double)Math.round(totalBillWithTip*100)/100;
+        double roundTipPerPerson = (double)Math.round(tipPerPerson*100)/100;
+        double roundTotalPerPerson = (double)Math.round(totalPerPerson*100)/100;
+        System.out.println("Total tip: $" + roundTotalTip);
+        System.out.println("Total bill including tip: $" + roundTotalBillWithTip);
+        System.out.println("Tip per person: $" + roundTipPerPerson);
+        System.out.println("Total per person: $" + roundTotalPerPerson);
 
 
 
